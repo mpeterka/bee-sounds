336047fb61e1ba2888adb58482d6a39ca072635b
LukasSirhal <sirhallukas@gmail.com>
2018-11-15 21:14:37 +0100

    Update avg Hz value from spectrum

--- spectro.py
+++ spectro.py
@@ -1,23 +1,41 @@
-import matplotlib
-import numpy as np
-from matplotlib import pyplot as plt
 from scipy import signal
 from scipy.io import wavfile
+import glob
+import numpy as np
+import matplotlib
 
 matplotlib.use('agg')
+from matplotlib import pyplot as plt
 
-rate, audio = wavfile.read('wav_data/18-08-07_09_10_12.wav')
 
-freqs, times, Sx = signal.spectrogram(audio,
-                                      fs=rate,
-                                      window='hanning',
-                                      nperseg=512,
-                                      noverlap=None,
-                                      detrend=False,
-                                      scaling='spectrum')
+def main():
+    files = glob.glob('wav_data/*.wav')
+    result = {}
+    for filename in files:
+        rate, audio = wavfile.read(filename)
+        freqs, times, Sx = signal.spectrogram(audio, fs=rate, window='hanning',
+                                              nperseg=1024,
+                                              detrend=False, scaling='density')
+        data_test = Sx
+        temp_res = []
+        for i in range(data_test.shape[0]):
+            s = 20 * np.log10(data_test[i])
+            hod = freqs[np.argmax(s)]
+            temp_res.append(hod)
 
-f, ax = plt.subplots(figsize=(9, 6))
-ax.pcolormesh(times, freqs, np.log10(Sx), cmap='viridis')
-ax.set_ylabel('Frequency [kHz]')
-ax.set_xlabel('Time [s]')
-plt.savefig('target/spectro.png')
+        if len(temp_res) > 0:
+            avg = np.average(temp_res)
+            print(avg)
+            result[filename] = avg
+
+    f, ax = plt.subplots(figsize=(9, 6))
+    ax.set_ylabel('bzucak')
+    ax.set_xlabel('files')
+    lists = sorted(result.items())
+    x, y = zip(*lists)
+    plt.plot(x, y)
+    plt.savefig('target/spectroFig.png')
+
+
+if __name__ == "__main__":
+    main()
